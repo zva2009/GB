5820952919c845ec2bdced6e8441f1fcfbf2eab6
Vadim <zva2009@live.ru>
2022-08-29 15:02:59 +0700

    Решение задачи

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+ 
+    if(num1 > num2)
+        {
+            Console.WriteLine("Наибольшее число =  " +num1);
+            Console.WriteLine("Наименьшее число =  " +num2);
+
+        }
+    else if (num1 < num2)
+        {
+            Console.WriteLine("Наибольшее число =  " +num2);
+            Console.WriteLine("Наименьшее число =  " +num1);
+        }
+            
